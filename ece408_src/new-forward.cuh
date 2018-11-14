dc5aa1cfd5bcb46d28eee5658ece770b0b6a9155
NeoWu1216 <twu54@illinois.edu>
2018-11-14 12:05:58 -0600

    Finished coding part of milestone 3

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -1,6 +1,7 @@
-
 #ifndef MXNET_OPERATOR_NEW_FORWARD_CUH_
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
+
+#define BLOCK_SIZE = 1024
 
 #include <mxnet/base.h>
 
@@ -21,8 +22,6 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
 
 // An example use of these macros:
 // float a = y4d(0,0,0,0)
@@ -30,6 +29,25 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+	
+	const int TILE_WIDTH = 16;
+	const int W_grid = ceil(1.*W_out/TILE_WIDTH);
+	const int H_grid = ceil(1.*H_out/TILE_WIDTH);
+
+	int b, m, h, w, c, p, q;
+	b = blockIdx.x;
+	m = blockIdx.y;
+	h = blockIdx.z / W_grid * TILE_WIDTH + threadIdx.y;
+	w = blockIdx.z % W_grid * TILE_WIDTH + threadIdx.x;
+	if (h >= H_out || w >= W_out)
+		return;
+	float acc = 0;
+	for (c = 0; c < C; c++) // sum over all input channels
+		for (p = 0; p < K; p++) // loop over KxK filter
+			for (q = 0; q < K; q++)
+				acc += x4d(b, c, h + p, w + q) * k4d(m, c, p, q);
+	y4d(b, m, h, w) = acc;
+
 
     
 
@@ -44,22 +62,37 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
    For ECE408, we only expect the float version of the operator to be called, so here we specialize with only floats.
 */
 template <>
-void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &w)
+void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &k)
 {
 
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
-    CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
+    // CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
     // ...
+    const int B = x.shape_[0];
+    const int M = y.shape_[1];
+    const int C = x.shape_[1];
+    const int H = x.shape_[2];
+    const int W = x.shape_[3];
+    const int K = k.shape_[3];
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
 
     // Set the kernel dimensions
-    // dim3 gridDim(0);
-    // dim3 blockDim(0);
+    const int TILE_WIDTH = 16;
+
+    // number of horizontal and vertical tiles per output map
+	const int W_grid = ceil(1.*W_out/TILE_WIDTH);
+	const int H_grid = ceil(1.*H_out/TILE_WIDTH);
+
+	int Z = H_grid * W_grid;
+	dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
+	dim3 gridDim(B, M, Z);
 
     // Call the kernel
-    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,k.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
